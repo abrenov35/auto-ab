baff19389c508141a8993031efcda90e22a59c94
abrenov35 <abrenov35@gmail.com>
2026-07-27 20:09:02 +0200

    v6.3.111: Ultra-simple GAS

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch');
 const app = express();
 
 // Configuration
-const GAS_URL = "https://script.google.com/macros/s/AKfycbyV-Gs2ZD5ugbye_GvazODtEJV_A3StcO_pabcPwf0Do1DTIAtazuRff6mqa8BKeKTL/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbz6m4BJNmb93r2rMgc8haiHBe5Ee7DLTWLf84DszQni31QPulDS9bKdOawe3CKFyQBm/exec";
 
 // Middleware: Parse JSON
 app.use(express.json({ limit: '50mb' }));
