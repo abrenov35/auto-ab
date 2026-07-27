25aa2761b8a08250382180287a8226c0166d300d
abrenov35 <abrenov35@gmail.com>
2026-07-27 18:03:02 +0200

    Fix: Update GAS URL to working deployment with all functions

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch');
 const app = express();
 
 // Configuration
-const GAS_URL = "https://script.google.com/macros/s/AKfycbzPLNDHUlMfPyTm95SK82rPNjxK5lV-JQSTr18TsaUeaTC8BRHIxfYt2zTkYzwCfZUl/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbzDYLcONQwTfydpyV__-ilBFAU96FJKo9kus-hYX1GvEGAtvrb0Q6kcTa6P0yGjwyW5/exec";
 
 // Middleware: Parse JSON
 app.use(express.json({ limit: '50mb' }));
