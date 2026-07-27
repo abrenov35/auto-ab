bf97334cec0d7935cee8b761e83cd17d1f8ad65e
abrenov35 <abrenov35@gmail.com>
2026-07-27 18:25:24 +0200

    Revert to working GAS URL

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch');
 const app = express();
 
 // Configuration
-const GAS_URL = "https://script.google.com/macros/s/AKfycbw-7bVbaFxHZ6OoiJ_HI5QYGWRn6Je9R99U0_BlAIdGvoPZ2DyYfmewlZM1Qfr7GYCZ/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbwG__1dk5qAN9axbr3q6NzN0MqaaHeDJJOPUUcs599FGDtYTgE3BkwUep-VF0fQQMyY/exec";
 
 // Middleware: Parse JSON
 app.use(express.json({ limit: '50mb' }));
